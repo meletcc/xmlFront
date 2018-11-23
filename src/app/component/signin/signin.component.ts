3d2a8941c8c529dacb88a8e3c8737865dff581f1
Angular <624244232@qq.com>
2018-11-23 09:47:13 +0800

    密码登录

--- src/app/component/signin/signin.component.ts
+++ src/app/component/signin/signin.component.ts
@@ -26,16 +26,16 @@ export class SigninComponent implements OnInit {
   signinSub() {
     this.loginSvr.login(this.signinForm.name, this.signinForm.pwd)
       .subscribe((data: any) => {
-        // if (data.code === 0) {
-        //   // 状态码为 0 表示登录成功，把 token 共享出去
-        //   window.sessionStorage.setItem('auto_token', data.data.token);
-        //   // 对象存储要转成字符串
-        //   window.sessionStorage.setItem('user', JSON.stringify(data.data.name));
-        //   alert('登录成功!');
-        // } else {
-        //   alert(data.msg);
-        // }
-        // this.router.navigate(['/']);
+        if (data.code === 0) {
+          // 状态码为 0 表示登录成功，把 token 共享出去
+          window.sessionStorage.setItem('auto_token', data.data.token);
+          // 对象存储要转成字符串
+          window.sessionStorage.setItem('user', JSON.stringify(data.data.name));
+          alert('登录成功!');
+        } else {
+          alert(data.msg);
+        }
+        this.router.navigate(['/']);
       }, err => {
         console.log(err);
       });
